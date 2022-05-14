667099cf999dbc20e03aff9ecc2b49e0732d9bfd
Alessandro Daniele Lamperti <alessandrodlamperti@yahoo.it>
2022-05-14 17:51:43 +0200

    Creazione della funzione di login dell'utente e varie possibilità di mancato collegamento oppure sbagliata email o password

--- app/src/main/java/it/uninsubria/biblioteca_app/Pagina_Login.kt
+++ app/src/main/java/it/uninsubria/biblioteca_app/Pagina_Login.kt
@@ -5,6 +5,10 @@ import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import android.app.ProgressDialog
 import android.content.Intent
+import android.os.PatternMatcher
+import android.text.TextUtils
+import android.util.Patterns
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
 import it.uninsubria.biblioteca_app.databinding.AppLoginBinding
 
@@ -19,6 +23,9 @@ class Pagina_Login : AppCompatActivity() {
     private lateinit var processDialog: ProgressDialog
     //FirebaseAuth
     private lateinit var firebaseAuth: FirebaseAuth
+    //Email e Password
+    private var email = ""
+    private var password = ""
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -44,6 +51,58 @@ class Pagina_Login : AppCompatActivity() {
         binding.Registrati.setOnClickListener {
             startActivity(Intent(this,Pagina_Registrazione::class.java))
         }
+
+        //Login
+        binding.Accedi.setOnClickListener {
+            //Prima del login
+            validateData()
+        }
+
+    }
+
+    private fun validateData() {
+        //Prendi i dati
+        email = binding.emailEt.text.toString().trim()
+        password = binding.passwordEt.text.toString().trim()
+
+        //Controllo dati
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            //Email non valida
+            binding.emailEt.error = "Formato Email non valido!"
+        }
+        else if(TextUtils.isEmpty(password)){
+            //Nessuna password immessa
+            binding.passwordEt.error = "Inserisci la Password"
+        }
+        else{
+            //Dati in ingresso sono corretti
+            firebaseLogin()
+        }
+
+
+    }
+
+    private fun firebaseLogin() {
+        //Mostra progress
+        processDialog.show()
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+            .addOnSuccessListener{
+                //Login Effettuato
+                processDialog.dismiss()
+                //Prendi Utente
+                val firebaseUser = firebaseAuth.currentUser
+                val email = firebaseUser!!.email
+                Toast.makeText(this, "Login da $email", Toast.LENGTH_SHORT).show()
+
+                //Apre la Homepage
+                startActivity(Intent(this,HomePage::class.java))
+                finish()
+            }
+            .addOnFailureListener { e->
+                //Login Saltato
+                processDialog.dismiss()
+                Toast.makeText(this, "login Fallito per causa di ${e.message}", Toast.LENGTH_SHORT).show()
+            }
 
     }
 
