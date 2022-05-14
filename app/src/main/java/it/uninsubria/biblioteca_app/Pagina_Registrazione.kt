f7a6bf4777424262e2db558b51222038c618b25d
Alessandro Daniele Lamperti <alessandrodlamperti@yahoo.it>
2022-05-14 16:15:24 +0200

    Aggiunta Grassetto e creazione pagina registrazione

---
+++ app/src/main/java/it/uninsubria/biblioteca_app/Pagina_Registrazione.kt
@@ -0,0 +1,11 @@
+package it.uninsubria.biblioteca_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Pagina_Registrazione : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_pagina_registrazione)
+    }
+}
