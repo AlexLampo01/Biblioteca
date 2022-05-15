399c9dc359a6da5511544f7f7b6dd8244a4e7a0d
Alessandro Daniele Lamperti <alessandrodlamperti@yahoo.it>
2022-05-15 14:23:28 +0200

    Creazione della activity HomePage e risolti alcuni Bug

--- app/src/main/java/it/uninsubria/biblioteca_app/HomePage.kt
+++ app/src/main/java/it/uninsubria/biblioteca_app/HomePage.kt
@@ -1,11 +1,57 @@
 package it.uninsubria.biblioteca_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import com.google.firebase.auth.FirebaseAuth
+import it.uninsubria.biblioteca_app.databinding.ActivityHomePageBinding
+import it.uninsubria.biblioteca_app.databinding.AppLoginBinding
 
 class HomePage : AppCompatActivity() {
+    //ViewBinding
+    private lateinit var binding: ActivityHomePageBinding
+
+    //ActionBar
+    private lateinit var actionBar: ActionBar
+
+    //FireBaseAuth
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home_page)
+        binding = ActivityHomePageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Configurazione ActionBar
+        actionBar = supportActionBar!!
+        actionBar.title = "Profilo"
+
+        //Configurazione Firebaseauth
+        firebaseAuth = FirebaseAuth.getInstance()
+        checkUser()
+
+        //Pulsante di uscita
+        binding.Esci.setOnClickListener {
+            firebaseAuth.signOut()
+            checkUser()
+        }
+
+    }
+
+    private fun checkUser() {
+        //Controlla che l'utente sia connesso
+        val firebaseUser = firebaseAuth.currentUser
+        if(firebaseUser != null){
+            //Utente collegato
+            val email = firebaseUser.email
+            //set to text view
+            binding.emailTv.text = email
+
+        }else{
+            //Utente non collegato
+            startActivity(Intent(this,Pagina_Login::class.java))
+            finish()
+        }
     }
 }
